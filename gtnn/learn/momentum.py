0a3b8c849d2480312e2b2aea0e00c1c2e4950905
Jan Olszak <janekolszak@gmail.com>
2014-04-27 22:33:14 +0200

    Momentum learning

---
+++ gtnn/learn/momentum.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def train(net, inputs, outputs, numEpochs=100, learningRate=0.1, momentum=0.6):
+    inputs = np.array(inputs)
+    outputs = np.array(outputs)
+
+    prevWeightChangeProp = net.addEdgeProperty("previous weight change",
+                                               "long double")
+
+    prevBiasChangeProp = net.addVertexProperty("previous bias change",
+                                             "long double")
+    epochIdx = 0
+    while True:
+        for inp, out in zip(inputs, outputs):
+            netOut = net.forward(inp)
+            err = netOut - out
+            net.backward(err)
+
+            # Weights learning:
+            for e in net.g.edges():
+                prevWeightChangeProp[e] = net.valueProp[e.target()] * \
+                    learningRate * net.errorProp[e.target()] + \
+                    prevWeightChangeProp[e] * momentum
+
+                net.weightProp[e] += prevWeightChangeProp[e]
+
+            # Bias learning
+            for v in net.g.vertices():
+                prevBiasChangeProp[v] = learningRate * net.errorProp[v] + \
+                    momentum * prevBiasChangeProp[v]
+
+                net.biasProp[v] += prevBiasChangeProp[v]
+
+        # End condition
+        epochIdx += 1
+        if epochIdx > numEpochs:
+            break
--- gtnn_tests/test_momentum.py
+++ gtnn_tests/test_momentum.py
@@ -4,10 +4,10 @@ from numpy.testing import assert_allclose
 import graph_tool.all as gt
 
 from gtnn.generators.multilayer_perceptron import multilayer_perceptron
-from gtnn.learn.simple import train
+from gtnn.learn.momentum import train
 
 
-class TestSimple(unittest.TestCase):
+class TestMomentum(unittest.TestCase):
 
     def test_simpleChain(self):
         inp = [[1]]
@@ -28,12 +28,11 @@ class TestSimple(unittest.TestCase):
         print(n.forward([1]))
         assert_allclose([n.forward(i) for i in inp], out)
 
-    # def test_xor(self):
-    #     inp = [[0, 0], [1, 0], [0, 1], [1, 1]]
-    #     out = [[0], [1], [1], [0]]
+    def test_xor(self):
+        inp = [[0, 0], [1, 0], [0, 1], [1, 1]]
+        out = [[0], [1], [1], [0]]
 
-    #     n = multilayer_perceptron([2, 2, 1])
-
-    #     train(net=n, inputs=inp, outputs=out,
-    #           numEpochs=1000, learningRate=0.1)
-    #     assert_allclose([n.forward(i) for i in inp], out)
+        n = multilayer_perceptron([2, 2, 1])
+        train(net=n, inputs=inp, outputs=out,
+              numEpochs=1000, learningRate=0.3, momentum=0.8)
+        assert_allclose([n.forward(i) for i in inp], out)
