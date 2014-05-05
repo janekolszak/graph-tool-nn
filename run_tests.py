4a3056b1cf8ea8f2a456a79271f022ac0c1486ec
Jan Olszak <janekolszak@gmail.com>
2014-05-05 22:46:21 +0200

    Changes in setup.py script

--- run_tests.py
+++ run_tests.py
@@ -3,5 +3,3 @@ import unittest
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().discover('.')
     unittest.TextTestRunner(verbosity=1).run(testsuite)
-
-
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='graph-tool-nn',
     version='0.0.1',
-    packages=['gtnn', ],
+    packages=['gtnn', 'gtnn.generators', 'gtnn.learn', 'gtnn.network'],
     license='GNU General Public License v3 (GPLv3)',
     long_description=open('README.md').read(),
 
